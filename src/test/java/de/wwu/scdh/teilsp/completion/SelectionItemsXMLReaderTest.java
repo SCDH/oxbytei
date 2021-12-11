0a431f4004bff095cab6633d017a45fc634c2a34
Christian Lück <christian.lueck@uni-muenster.de>
2021-12-11 12:36:59 +0100

    test for reading tei with and without namespace parameter

--- src/test/java/de/wwu/scdh/teilsp/completion/SelectionItemsXMLReaderTest.java
+++ src/test/java/de/wwu/scdh/teilsp/completion/SelectionItemsXMLReaderTest.java
@@ -3,6 +3,7 @@ package de.wwu.scdh.teilsp.completion;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Disabled;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
@@ -10,6 +11,9 @@ import java.nio.file.Paths;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import de.wwu.scdh.teilsp.exceptions.DocumentReaderException;
+
 
 public class SelectionItemsXMLReaderTest {
 
@@ -33,16 +37,41 @@ public class SelectionItemsXMLReaderTest {
 
     @Test
     @DisplayName("Test with teigraphy.xml")
-    void testRegistry() {
-	try {
-	    FileInputStream input = new FileInputStream(teigraphy);
-	    System.out.println(input);
-	    // add tests here
-	    input.close();
-	} catch (FileNotFoundException e) {
-	} catch (IOException e) {
-	}
+    void testRegistry()
+	throws FileNotFoundException, IOException, DocumentReaderException {
+	FileInputStream input = new FileInputStream(teigraphy);
+	System.out.println(input);
+	PrefixDef prefix = new PrefixDef("([a-zA-Z0-9_-]+)", "teigraphy.xml#$1", "psn");
+	reader = new SelectionItemsXMLReader(prefix, input,
+					     "//t:person", "@xml:id", "*", "t:http://www.tei-c.org/ns/1.0");
+	assertEquals(3, reader.nodesCount());
+	input.close();
+    }
 
+    @Test
+    @DisplayName("Test with teigraphy.xml with empty namespace")
+    void testRegistryEmptyNamespace()
+	throws FileNotFoundException, IOException, DocumentReaderException {
+	FileInputStream input = new FileInputStream(teigraphy);
+	System.out.println(input);
+	PrefixDef prefix = new PrefixDef("([a-zA-Z0-9_-]+)", "teigraphy.xml#$1", "psn");
+	assertThrows(StringIndexOutOfBoundsException.class,
+		     () -> new SelectionItemsXMLReader(prefix, input, "//person", "@xml:id", "*", ""));
+	input.close();
+    }
+
+    @Test
+    @Disabled("This test is pending")
+    @DisplayName("Test with teigraphy.xml with default namespace")
+    void testRegistryDefaultNamespace()
+	throws FileNotFoundException, IOException, DocumentReaderException {
+	FileInputStream input = new FileInputStream(teigraphy);
+	System.out.println(input);
+	PrefixDef prefix = new PrefixDef("([a-zA-Z0-9_-]+)", "teigraphy.xml#$1", "psn");
+	reader = new SelectionItemsXMLReader(prefix, input,
+					     "//person", "@xml:id", "*", "http://www.tei-c.org/ns/1.0");
+	assertEquals(3, reader.nodesCount());
+	input.close();
     }
 
 }
