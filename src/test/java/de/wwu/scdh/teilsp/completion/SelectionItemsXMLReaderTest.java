a430f3e26e1b3573a42041ff8182f174e94f1f46
Christian Lück <christian.lueck@uni-muenster.de>
2021-12-12 23:10:42 +0100

    test labels

--- src/test/java/de/wwu/scdh/teilsp/completion/SelectionItemsXMLReaderTest.java
+++ src/test/java/de/wwu/scdh/teilsp/completion/SelectionItemsXMLReaderTest.java
@@ -20,11 +20,13 @@ public class SelectionItemsXMLReaderTest {
 
     String resources;
     String teigraphy;
+    String teigraphyNs;
 
     @BeforeEach
     void setup() {
 	resources = Paths.get("src", "test", "resources").toFile().getAbsolutePath();
 	teigraphy = Paths.get("src", "test", "resources", "teigraphy.xml").toFile().getAbsolutePath();
+	teigraphyNs = "t:http://www.tei-c.org/ns/1.0 xml:http://www.w3.org/XML/1998/namespace";
     }
 
     @Test
@@ -39,13 +41,12 @@ public class SelectionItemsXMLReaderTest {
     void testRegistryKeys()
 	throws FileNotFoundException, IOException, DocumentReaderException {
 	FileInputStream input = new FileInputStream(teigraphy);
-	System.out.println(input);
 	PrefixDef prefix = new PrefixDef("([a-zA-Z0-9_-]+)", "teigraphy.xml#$1", "psn");
 	reader = new SelectionItemsXMLReader(prefix, input,
 					     "//t:person",
-					     "$XPATH{@xml:id}",
-					     "$XPATH{'const'}",
-					     "t:http://www.tei-c.org/ns/1.0");
+					     "@xml:id",
+					     "'const'",
+					     teigraphyNs);
 	assertEquals(3, reader.nodesCount());
 	assertEquals(3, reader.getLength());
 	LabelledEntry[] entries = reader.getEntries();
@@ -53,11 +54,12 @@ public class SelectionItemsXMLReaderTest {
 	assertEquals("psn:FCSavigny", entries[0].getKey());
 	assertEquals("psn:Puchta", entries[1].getKey());
 	assertEquals("psn:JGrimm", entries[2].getKey());
+	// test the constant xpath expression for the label:
+	assertEquals("const", entries[0].getLabel());
 	input.close();
     }
 
     @Test
-    @Disabled("This test is pending: Labels do not work yet.")
     @DisplayName("Test labels with teigraphy.xml")
     void testRegistryLabels()
 	throws FileNotFoundException, IOException, DocumentReaderException {
@@ -66,14 +68,16 @@ public class SelectionItemsXMLReaderTest {
 	PrefixDef prefix = new PrefixDef("([a-zA-Z0-9_-]+)", "teigraphy.xml#$1", "psn");
 	reader = new SelectionItemsXMLReader(prefix, input,
 					     "//t:person",
-					     "$XPATH{@xml:id}",
-					     "$XPATH{normalize-space(t:persName)}",
-					     "t:http://www.tei-c.org/ns/1.0");
+					     "@xml:id",
+					     "normalize-space(t:persName)",
+					     teigraphyNs);
 	assertEquals(3, reader.nodesCount());
 	assertEquals(3, reader.getLength());
 	LabelledEntry[] entries = reader.getEntries();
 	assertEquals(3, entries.length);
-	assertEquals("Friedrich Carl von Savigny ", entries[0].getLabel());
+	assertEquals("Friedrich Carl von Savigny", entries[0].getLabel());
+	assertEquals("Georg Friedrich Puchta", entries[1].getLabel());
+	assertEquals("Jacob Grimm", entries[2].getLabel());
 	input.close();
     }
 
