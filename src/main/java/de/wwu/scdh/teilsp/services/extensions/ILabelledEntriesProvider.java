be5ca5bc167e4898b8d429f0ec9ef687023d5be6
Christian Lück <christian.lueck@uni-muenster.de>
2021-12-18 18:01:27 +0100

    changed interface to allow stateful plugins
    According to the new interface, the plugins have an initilization
    phase and a query phase. The query triggered by getLabelledEntries()
    may be called several times. So the plugin has a state.

--- src/main/java/de/wwu/scdh/teilsp/services/extensions/ILabelledEntriesProvider.java
+++ src/main/java/de/wwu/scdh/teilsp/services/extensions/ILabelledEntriesProvider.java
@@ -1,17 +1,42 @@
+/**
+ * {@link ILabelledEntriesProvider} - an interface for plugins, that
+ * provide labelled entries.
+ *
+ */
 package de.wwu.scdh.teilsp.services.extensions;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import javax.xml.transform.URIResolver;
+import org.xml.sax.EntityResolver;
 
 
 public interface ILabelledEntriesProvider {
 
     /**
+     * This method initializes the provider with a setup from
+     * configuration and the current editing context. It is once
+     * called during the initialization phase.
+     *
+     * @param  kwargs      key value pairs with configuration parameters
+     * @param  uriResolver URI resolver used in the editor
+     * @param  entityResolver entity resolver used in the editor
+     * @param  systemId    URL of the currently edited file
+     * @return             an ordered sequence of labelled entries
+     */
+    void init(Map<String, String> kwargs,
+	      URIResolver uriResolver,
+	      EntityResolver entityResolver,
+	      URL systemId) throws ExtensionException;
+    
+    /**
      * This method must return an ordered collection of labelled
      * entries. It takes a map of key value pairs as arguments.
-     * @param  kwargs   key value pairs with configuration parameters
-     * @return          an ordered sequence of labelled entries
+     *
+     * @param  userInput   what the user has typed so far	
+     * @return             an ordered sequence of labelled entries
      */
-    List<LabelledEntry> getLabelledEntries(Map<String, String> kwargs) throws ExtensionException;
+    List<LabelledEntry> getLabelledEntries(String userInput) throws ExtensionException;
     
 }
