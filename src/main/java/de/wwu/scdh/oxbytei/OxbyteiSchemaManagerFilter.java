0cb569ea76b7d9258723965bb5f89930e1b6094a
Christian Lück <christian.lueck@uni-muenster.de>
2021-12-27 17:19:18 +0100

    move logic for getting providers over to teilsp

--- src/main/java/de/wwu/scdh/oxbytei/OxbyteiSchemaManagerFilter.java
+++ src/main/java/de/wwu/scdh/oxbytei/OxbyteiSchemaManagerFilter.java
@@ -1,13 +1,12 @@
 package de.wwu.scdh.oxbytei;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 import javax.xml.transform.URIResolver;
 
 import org.xml.sax.EntityResolver;
+
+import org.w3c.dom.Document;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,15 +20,11 @@ import ro.sync.contentcompletion.xml.WhatAttributesCanGoHereContext;
 import ro.sync.contentcompletion.xml.WhatElementsCanGoHereContext;
 import ro.sync.contentcompletion.xml.WhatPossibleValuesHasAttributeContext;
 import ro.sync.exml.workspace.api.PluginWorkspaceProvider;
-import ro.sync.exml.workspace.api.PluginWorkspace;
 
 import de.wwu.scdh.teilsp.services.extensions.ILabelledEntriesProvider;
 import de.wwu.scdh.teilsp.services.extensions.LabelledEntriesLoader;
 import de.wwu.scdh.teilsp.services.extensions.LabelledEntry;
 import de.wwu.scdh.teilsp.services.extensions.ExtensionException;
-import de.wwu.scdh.teilsp.config.ArgumentsConditionsPair;
-import de.wwu.scdh.teilsp.config.ExtensionConfiguration;
-import de.wwu.scdh.teilsp.config.ExtensionConfigurationReader;
 import de.wwu.scdh.teilsp.exceptions.ConfigurationException;
 
 
@@ -46,7 +41,50 @@ public class OxbyteiSchemaManagerFilter
     // {"xml", "http://www.w3.org/XML/1998/namespace",
     //  "tei", "http://www.tei-c.org/ns/1.0",
     //  "", "http://www.tei-c.org/ns/1.0"};
-    
+
+    protected List<ILabelledEntriesProvider> providersForContext(Context context, String nodeType, String nodeName) {
+	// get resolvers and url of the current editing context
+	URIResolver uriResolver =
+	    PluginWorkspaceProvider.getPluginWorkspace().getXMLUtilAccess().getURIResolver();
+	EntityResolver entityResolver =
+	    PluginWorkspaceProvider.getPluginWorkspace().getXMLUtilAccess().getEntityResolver();
+
+	// get the URL of the configuration file
+	String configFile = OxbyteiConstants.getConfigFile();
+
+	try {
+	    // FIXME: the resulting DocumentOverNodeInfo fails with further evalution
+	    List<Object> docNodes = context.executeXPath(OxbyteiConstants.DOCUMENT_XPATH, namespaces, true);
+	    String ctx =
+		context.computeContextXPathExpression().replaceAll("/", "/*:"); //.substring(1);
+
+	    LOGGER.debug("Count of docNodes: {}", docNodes.size());
+	    LOGGER.debug("Type of first in docNodes: {}, name of root element: {}",
+			 docNodes.get(0).getClass().getCanonicalName(),
+			 ((Document) docNodes.get(0)).getDocumentElement().getLocalName());
+
+	    // get the initialized providers
+	    List<ILabelledEntriesProvider> providers =
+		LabelledEntriesLoader.providersForContext((Document) docNodes.get(0),
+							  context.getSystemID(),
+							  ctx,
+							  nodeType,
+							  nodeName,
+							  uriResolver,
+							  entityResolver,
+							  null,
+							  configFile);
+
+	    return providers;
+	} catch (ConfigurationException e) {
+	    LOGGER.error("{}", e);
+	} catch (ExtensionException e) {
+	    LOGGER.error("{}", e);
+	}
+	return null;
+
+    }
+
     @Override
     public List<CIValue> filterAttributeValues(List<CIValue> list, WhatPossibleValuesHasAttributeContext context) {
 
@@ -54,106 +92,11 @@ public class OxbyteiSchemaManagerFilter
 	String attributeName = context.getAttributeName();
 	String alreadyTyped = context.getCurrentValuePrefix();
 
-	// computeContextXPathExpression() is present according to compiler
-	String contextXPath = context.computeContextXPathExpression();
-
-	// Load providers
-	List<ILabelledEntriesProvider> entriesProviders = LabelledEntriesLoader.providers();
-
-	// get resolvers and url of the current editing context
-	URIResolver resolver =
-	    PluginWorkspaceProvider.getPluginWorkspace().getXMLUtilAccess().getURIResolver();
-	EntityResolver entityResolver =
-	    PluginWorkspaceProvider.getPluginWorkspace().getXMLUtilAccess().getEntityResolver();
-	URL currentFileURL =
-	    PluginWorkspaceProvider.getPluginWorkspace().getCurrentEditorAccess(PluginWorkspace.MAIN_EDITING_AREA).getEditorLocation();
-
-	// get the URL of the configuration file
-	String configFile = OxbyteiConstants.getConfigFile();
-
-	// read the plugin configuration from the config file
-	List<ExtensionConfiguration> extensionsConfiguration = new ArrayList<ExtensionConfiguration>();
-	try {
-	    extensionsConfiguration = ExtensionConfigurationReader.getExtensionsConfiguration(configFile);
-	} catch (ConfigurationException e) {
-	    LOGGER.error("Error reading config from '{}'\nDetails:\n{}", configFile, e);
-	}
-
-	// we need some iteration variables
-	int i, j, k;
-
-	// get plugins configured for current editing context
-	ExtensionConfiguration config;
-	List<ILabelledEntriesProvider> configuredEntriesProviders = new ArrayList<ILabelledEntriesProvider>();
-	//System.err.println("plugin configurations: " + extensionsConfiguration.size());
-	// iterate over extension (plugin) configurations from config file
-	for (i = 0; i < extensionsConfiguration.size(); i++) {
-	    config = extensionsConfiguration.get(i);
-	    // iteratre over loaded plugins
-	    for (j = 0; j < entriesProviders.size(); j++) {
-		ILabelledEntriesProvider entriesProvider = entriesProviders.get(j);
-		// check if the configuration is for this provider
-		if (entriesProvider.getClass().getCanonicalName().equals(config.getClassName())) {
-		    // check all specifications of this provider
-		    for (k = 0; k < config.getSpecification().size(); k++) {
-			ArgumentsConditionsPair spec = config.getSpecification().get(k);
-			// prepare an error message we might throw multiple times
-			LOGGER.debug("Running XPath configured as 'context' condition for {} in config file {}:\n{}",
-				     config.getClassName(),
-				     configFile,
-				     spec.getConditions().get("context"));
-			// check condition defined in 'context' matches the current edition context
-			try {
-			    // run xpath configured as context on the current editing context
-			    // note: we put the context form conditions into an xpath attribute!
-			    String xpath = contextXPath + "[" + spec.getConditions().get("context") + "]";
-			    LOGGER.debug(xpath);
-			    List<Object> contxt = context.executeXPath(xpath, namespaces, false);
-			    LOGGER.debug("This plugin matches: {}", contxt.size());
-			    if (// matching context?
-				contxt.size() > 0
-				// configured for attribute?
-				&& spec.getConditions().get("nodeType").equals("attribute")
-				// matching attribute name?
-				&& spec.getConditions().get("nodeName").equals(attributeName)
-				) {
-				// we make a new instance of the map, because we set some values of it
-				Map<String, String> arguments = new HashMap<String, String>(spec.getArguments());
-				// we make a new instance of the
-				// provider, because we do not want to
-				// configure the same several times.
-				ILabelledEntriesProvider p = entriesProvider.getClass().newInstance();
-				p.init(arguments, resolver, entityResolver, currentFileURL);
-				configuredEntriesProviders.add(p);
-			    }
-			} catch (NullPointerException e) {
-			    LOGGER.error("Configuration error in {}\n{}", configFile, e);
-			} catch (InstantiationException e) {
-			    LOGGER.error("Error loading plugin {}\n{}",
-					 entriesProvider.getClass().getCanonicalName(),
-					 e);
-			} catch (IllegalAccessException e) {
-			    LOGGER.error("Error loading plugin {}\n",
-					 entriesProvider.getClass().getCanonicalName(),
-					 e);
-			} catch (ExtensionException e) {
-				LOGGER.error("Error initializing plugin {} \nusing config file {}:\n",
-					     entriesProvider.getClass().getCanonicalName(),
-					     configFile, e);
-			}
-		    }
-		}
-	    }
-	}
-	LOGGER.debug("Configured plugins: {}", configuredEntriesProviders.size());
-	for (ILabelledEntriesProvider p : configuredEntriesProviders) {
-	    LOGGER.debug("Plugin {} configured with arguments:\n{}",
-			 p.toString(),
-			 p.getArguments().toString());
-	}
+	List<ILabelledEntriesProvider> providers =
+	    providersForContext(context, LabelledEntriesLoader.ATTRIBUTE_VALUE, attributeName);
 
 	List<CIValue> suggestions = new ArrayList<CIValue>();
-	for (ILabelledEntriesProvider provider : configuredEntriesProviders) {
+	for (ILabelledEntriesProvider provider : providers) {
 	    try {
 		List<LabelledEntry> entries = provider.getLabelledEntries(alreadyTyped);
 		for (LabelledEntry entry : entries) {
