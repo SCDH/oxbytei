168efa8f8834b5076ed772a9eecf096231738262
Christian Lück <christian.lueck@uni-muenster.de>
2021-12-30 18:08:33 +0100

    unit test of logic for selecting providers for an editing context

--- src/test/java/de/wwu/scdh/teilsp/services/extensions/LabelledEntriesLoaderTest.java
+++ src/test/java/de/wwu/scdh/teilsp/services/extensions/LabelledEntriesLoaderTest.java
@@ -1,14 +1,45 @@
 package de.wwu.scdh.teilsp.services.extensions;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
 import java.util.List;
+import java.nio.file.Paths;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
+import org.w3c.dom.Document;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import de.wwu.scdh.teilsp.exceptions.ConfigurationException;
 import de.wwu.scdh.teilsp.exceptions.ProviderNotFoundException;
+import de.wwu.scdh.teilsp.testutils.SimpleURIResolver;
 
 
 public class LabelledEntriesLoaderTest {
+
+    String configFile, currentFile;
+    Document document;
+
+    @BeforeEach
+    void setup() throws ParserConfigurationException, SAXException, IOException {
+	configFile = "file:" + Paths.get("src", "test", "resources", "config.xml").toFile().getAbsolutePath();
+	currentFile = "file:" + Paths.get("src", "test", "resources", "fatherandson.xml").toFile().getAbsolutePath();
+
+	// prepare dom builder
+	DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+	domFactory.setNamespaceAware(true);
+	DocumentBuilder builder = domFactory.newDocumentBuilder();
+	// parse the input document
+	InputSource inputSource = new InputSource(currentFile);
+	document = builder.parse(inputSource);
+    }
 
     @Test
     public void testAtLeastDefaultProvider() {
@@ -28,4 +59,18 @@ public class LabelledEntriesLoaderTest {
 		     () -> { LabelledEntriesLoader.provider("unknown");
 		     });
     }
+
+    @Test
+    public void testProvidersForContextPersName() throws ExtensionException, ConfigurationException {
+	List<ILabelledEntriesProvider> providers =
+	    LabelledEntriesLoader.providersForContext(document,
+						      currentFile,
+						      "/*:TEI[1]/*:text[1]/*:body[1]/*:p[1]/*:persName[1]",
+						      LabelledEntriesLoader.ATTRIBUTE_VALUE,
+						      "ref",
+						      new SimpleURIResolver(), null, null,
+						      configFile);
+	assertEquals(1, providers.size());
+    }
+
 }
