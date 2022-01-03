12ae6613040b3e867be34f0673b3fba5529ad4d1
Christian Lück <christian.lueck@uni-muenster.de>
2022-01-03 16:51:26 +0100

    added doc strings

--- src/main/java/de/wwu/scdh/oxbytei/InteractiveOperation.java
+++ src/main/java/de/wwu/scdh/oxbytei/InteractiveOperation.java
@@ -2,8 +2,17 @@ package de.wwu.scdh.oxbytei;
 
 import ro.sync.ecss.extensions.api.AuthorOperationException;
 
+/**
+ * An interface for an interactive (author mode) operation.
+ *
+ * @author Christian Lück
+ */
 public interface InteractiveOperation {
 
+    /**
+     * Calling {@code doUserInteraction()} actually does the user
+     * interaction.
+     */
     public String doUserInteraction() throws AuthorOperationException;
 
 }
