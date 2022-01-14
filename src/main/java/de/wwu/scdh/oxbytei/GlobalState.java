e04faacd4cf547c3490c85d8fbf8737ddb2aee7a
Christian Lück <christian.lueck@uni-muenster.de>
2022-01-14 19:58:00 +0100

    static class for storing state

---
+++ src/main/java/de/wwu/scdh/oxbytei/GlobalState.java
@@ -0,0 +1,14 @@
+package de.wwu.scdh.oxbytei;
+
+/**
+ * A static class for storing the framework's state.
+ */
+public class GlobalState {
+
+    public static String startAnchorId = null;
+
+    public static String endAnchorId = null;
+
+    public static String anchorsContainer = null;
+
+}
