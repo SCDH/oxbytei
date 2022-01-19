6c188f4f783fe1a4e9bc7c48bc50783ee51552c6
Christian Lück <christian.lueck@uni-muenster.de>
2022-01-19 21:39:58 +0100

    added doc strings

--- src/main/java/de/wwu/scdh/oxbytei/GlobalState.java
+++ src/main/java/de/wwu/scdh/oxbytei/GlobalState.java
@@ -5,12 +5,24 @@ package de.wwu.scdh.oxbytei;
  */
 public class GlobalState {
 
+    /**
+     * set by {@link SurroundWithAnchorsOperation}
+     */
     public static String startAnchorId = null;
 
+    /**
+     * set by {@link SurroundWithAnchorsOperation}
+     */
     public static String endAnchorId = null;
 
+    /**
+     * set by {@link SurroundWithAnchorsOperation}
+     */
     public static String anchorsContainer = null;
 
+    /**
+     * set by {@link SelectLabelledEntryInteraction}
+     */
     public static String selection = null;
 
 }
