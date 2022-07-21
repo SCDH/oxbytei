b2c176458f9f8d3bfea57ecc3b7842f4b798cbaa
Christian Lück <christian.lueck@uni-muenster.de>
2022-07-21 14:51:18 +0200

    encapsulation of our saxon configuration

---
+++ src/main/java/de/wwu/scdh/teilsp/extensions/SaxonConfiguration.java
@@ -0,0 +1,16 @@
+package de.wwu.scdh.teilsp.extensions;
+
+import net.sf.saxon.Configuration;
+
+/**
+ * Encapsulation of configuration settings.
+ */
+public class SaxonConfiguration extends Configuration {
+
+    public SaxonConfiguration() {
+	super();
+    }
+
+    // TODO: make xsl:evaluate available
+
+}
